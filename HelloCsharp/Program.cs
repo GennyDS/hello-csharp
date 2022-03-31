04c37261d842a3bb4ee949fff9ff445e1f07cbbd
Gennaro <gennarinodi02@gmail.com>
2022-03-31 14:55:46 +0200

    finito con ARREY

--- HelloCsharp/Program.cs
+++ HelloCsharp/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine();
+string[] mesi = {"Gennaio","Febbraio","Marzo","Aprile","Maggio","Giugno","Luglio","Agosto","Settembre","Ottobre","Novembre","Dicembre" };
+string mesePreferito;
+for (int i = 0; i < mesi.Length; i++)
+{ 
+Console.WriteLine(mesi[i]);
+}
+Console.WriteLine();
+Console.WriteLine("Qual è il tuo mese preferito ?");
+mesePreferito= Console.ReadLine();
+Console.WriteLine("il tuo mese preferito è: " +mesePreferito);
